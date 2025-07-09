0948230a9104bc714217f013ba6eb31cfeabd644
CagesThrottleUs <manstein.felix@gmail.com>
2025-07-09 05:54:11 +0530

    Refactor Codeforces 263A solution for clarity and maintainability
    - Introduced a namespace for constants to define matrix size and target position.
    - Renamed variables for better readability.
    - Updated loop structure to utilize constants for matrix dimensions.
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- CodeForces/263A/main.cpp
+++ CodeForces/263A/main.cpp
@@ -1,29 +1,44 @@
 #include <cstdlib>
 #include <iostream>
 
-int main() {
+namespace constants
+{
+constexpr int MATRIX_SIZE{5};
+constexpr int TARGET_ROW{3};
+constexpr int TARGET_COL{3};
+} // namespace constants
+
+auto main() -> int
+{
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-    int px{0}, py{0};
+    int pos_x{0};
+    int pos_y{0};
     int val{};
     bool signal{false};
 
-    for (int x=1; x<=5; x++) {
-        for (int y=1; y<=5; y++) {
+    for (int coord_x = 1; coord_x <= constants::MATRIX_SIZE; coord_x++)
+    {
+        for (int coord_y = 1; coord_y <= constants::MATRIX_SIZE; coord_y++)
+        {
             std::cin >> val;
-            if (val == 1) {
-                px = x;
-                py = y;
+            if (val == 1)
+            {
+                pos_x = coord_x;
+                pos_y = coord_y;
                 signal = true;
                 break;
             }
         }
-        if (signal) break;
+        if (signal)
+        {
+            break;
+        }
     }
 
-    int moves = std::abs(px - 3) + std::abs(py - 3);
+    int moves = std::abs(pos_x - constants::TARGET_ROW) + std::abs(pos_y - constants::TARGET_COL);
 
     std::cout << moves << '\n';
 
